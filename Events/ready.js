1101580c1cebee50b03c3e69c77b6a474561f3aa
Window-s <113310753+Window-s@users.noreply.github.com>
2022-12-02 21:20:51 +0530

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -3,18 +3,4 @@ const client = require("../index");
 
 client.on('ready', () => {
     console.log(`${client.user.tag} is now online`)
-    const channel = client.channels.cache.get('1035305773712019527');
-    channel.send(`${client.user} is online. LOOK OUT FOR BUGS AND USE \`/report\` TO SEND US A MESSAGE`);
-    
-    setInterval(() => {
-      const arrayOfStatus = [
-                'you 😳, WATCHING',
-                `${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)} users, WATCHING`
-        ]
-        const random = arrayOfStatus[Math.floor(Math.random() * arrayOfStatus.length)].split(', ')
-        const status = random[0]
-        const mode = random[1]
-        client.user.setActivity(status, {type: mode})
-    }, 10000)
-    
-})
+ })
